a7c38803639608b9b5858b5063296c83dd815755
Hareram Chua <hareram.chua@cit.edu>
2023-03-08 11:59:15 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Backprop;
+
+namespace Botters
+{
+    public partial class Form1 : Form
+    {
+        NeuralNet nn;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nn = new NeuralNet(2, 3, 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int x = 0; x < Convert.ToInt32(textBox4.Text); x++)
+            {
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 1.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+            nn.setInputs(1, Convert.ToDouble(textBox2.Text));
+            nn.run();
+            textBox3.Text = "" + nn.getOuputData(0);
+        }
+    }
+}
